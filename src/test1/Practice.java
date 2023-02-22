88a385de0f2b256b0c0b56c57c8e6ca71d78004f
johntandukar <john.tandukar@tekschool.us>
2023-02-21 20:08:46 -0500

    This is second Push

---
+++ src/test1/Practice.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("This is just a practice for pull");
+	}
+
+}
