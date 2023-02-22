729895381ae44d4795067ff0cc7ccff598d7f946
johntandukar <125515735+johntandukar@users.noreply.github.com>
2023-02-21 20:33:18 -0500

    Update Practice.java
    added substraction

--- src/test1/Practice.java
+++ src/test1/Practice.java
@@ -7,8 +7,10 @@ public class Practice {
      System.out.println("This is just a practice for pull");
 		System.out.println("This is new pull");
 		System.out.println(add(6,6));
+		System.out.println(sub(12,6);
 	}
  public static int add (int a, int b) {
 	 return a+b;
+	 return a-b;
  }
 }
